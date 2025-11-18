6e12fb619e48fa04b434fe4c9d75b03f7cbed658
Eriionn <ezeqirii00@gmail.com>
2025-11-18 17:59:36 +0100

    app fix
    i fixed app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
-import "./App.css"
+import "./App.css";
 import CustomCakePage from "./components/pages/CustomCakes/CakePage";
 import ContactFaqPage from "./components/pages/Contact";
 import SignInSignUp from "./components/pages/SignInSignUp";
@@ -11,25 +10,10 @@ export default function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< Updated upstream
-        <Route path="/custom-cakes" element={<CustomCakePage />} />
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         <Route path="/cake" element={<CustomCakePage />} />
+        <Route path="/custom-cakes" element={<CustomCakePage />} />
         <Route path="/contact" element={<ContactFaqPage />} />
         <Route path="/auth" element={<SignInSignUp />} />
-=======
-        <Route path="/custom-cakes" element={<CustomCakePage />} />
->>>>>>> 4757abde15850ed370c50c6284ed9725960045d9
-=======
-        <Route path="/custom-cakes" element={<CustomCakePage />} />
->>>>>>> 4757abde15850ed370c50c6284ed9725960045d9
-=======
-        <Route path="/custom-cakes" element={<CustomCakePage />} />
->>>>>>> 4757abde15850ed370c50c6284ed9725960045d9
->>>>>>> Stashed changes
         <Route path="/" element={<Home />} />
       </Routes>
     </Router>
