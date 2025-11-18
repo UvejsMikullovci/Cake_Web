df63101acd34a0bf70f659044dbc4ff20f0051cd
Uvejs Mikullovci <mikullovciuvejs@gmail.com>
2025-11-18 19:38:13 +0100

    a few more updates

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,20 +1,17 @@
-// src/components/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth"; // <-- import auth
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDlU191ihhs9NvL3j5LizVD-zBOxSYH-y8",
   authDomain: "cakeweb-eff60.firebaseapp.com",
   projectId: "cakeweb-eff60",
-  storageBucket: "cakeweb-eff60.appspot.com",  // <-- FIXED
+  storageBucket: "cakeweb-eff60.appspot.com",
   messagingSenderId: "431637329493",
   appId: "1:431637329493:web:0f655a9bc4b16a664cc6dc",
   measurementId: "G-YEETM4YJGG"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-export const auth = getAuth(app); // <-- export auth
+export const auth = getAuth(app);
