6facb57b7cbe33c31b80816ad1867a230edd5c4f
Eriionn <ezeqirii00@gmail.com>
2025-11-23 21:38:38 +0100

    new thing to payment

--- src/components/pages/PaymentsAndCheckout.js
+++ src/components/pages/PaymentsAndCheckout.js
@@ -234,8 +234,12 @@ export default function PaymentsAndCheckout() {
         <p>Almost there! Complete your order below</p>
       </div>
 
-      <svg className="svgBaner" viewBox="0 0 1200 120">
-        <path d="M321.39,56.44..." fill="#F7C0C3" />
+      {/* Wave Banner */}
+      <svg className="svgBaner" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path
+          d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"
+          fill="#F7C0C3"
+        ></path>
       </svg>
 
       <div className="checkout-container">
